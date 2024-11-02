0e5602c859746093b8d5551d5dfa259fb5b04389
v4n <105587619+v4n00@users.noreply.github.com>
2024-11-02 22:32:36 +0200

    fix: notifications

---
+++ hypr/.config/hypr/scripts/assets/change-color.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <new_color>"
+  echo "Example: $0 \"#ff0000\""
+  exit 1
+fi
+
+new_color=$1
+
+for svg_file in ./*.svg; do
+  sed -i "s/fill=\"#\([0-9a-fA-F]\{6\}\)\"/fill=\"$new_color\"/g" "$svg_file"
+  echo "Replaced color in $svg_file with $new_color"
+done
