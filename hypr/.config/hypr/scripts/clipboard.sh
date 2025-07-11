f0896549fa1a3487dc19965657ba101b9bd4f7b5
v4n <105587619+v4n00@users.noreply.github.com>
2025-07-11 20:52:04 +0300

    changed ss utility

--- hypr/.config/hypr/scripts/clipboard.sh
+++ hypr/.config/hypr/scripts/clipboard.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cliphist list | tofi --prompt-text "clip: " "$@" | cliphist decode | wl-copy
 
--- hypr/.config/hypr/scripts/screenshot-area.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-grimblast --notify --freeze copysave area $HOME"/Pictures/Screenshots/"$(date +'%F-%T.png')
-
