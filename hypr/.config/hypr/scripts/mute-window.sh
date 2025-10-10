279fdb94058ea0edb1431b795eba57b924f68bde
v4n <105587619+v4n00@users.noreply.github.com>
2025-10-10 23:31:20 +0300

    fix mute windows

--- hypr/.config/hypr/scripts/mute-window.sh
+++ hypr/.config/hypr/scripts/mute-window.sh
@@ -7,6 +7,15 @@ SINKS=$(pactl list sink-inputs | awk -v pid="$ACTIVE_PID" '
 /Mute:/ {mute=$2}
 /application.process.id/ && $0 ~ pid {print id "," mute}')
 
+if [ -z "$SINKS" ]; then
+    APP_NAME=$(ps -p $ACTIVE_PID -o comm=)
+    SINKS=$(pactl list sink-inputs | awk -v app="$APP_NAME" '
+/Sink Input/ {id=$3; sub(/^#/, "", id); mute=""; next}
+/Mute:/ {mute=$2}
+/application.name/ && tolower($0) ~ tolower(app) {print id "," mute}
+')
+fi
+
 for sink in $SINKS; do
     ID=$(echo "$sink" | cut -d',' -f1)
     STATUS=$(echo "$sink" | cut -d',' -f2)
